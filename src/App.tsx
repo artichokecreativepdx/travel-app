c6163259b07a84ce213f0cf23e3cdc0f9a02e41d
megan <me.maldy@gmail.com>
2021-11-09 16:48:54 -0800

    switching to tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Redirect, Route } from 'react-router-dom';
-import {
+
+import { 
   IonApp,
   IonIcon,
   IonLabel,
@@ -9,14 +10,11 @@ import {
   IonTabs,
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { personSharp, imageSharp, settingsSharp} from 'ionicons/icons';
-import Tab1 from './pages/Tab1';
-import Tab2 from './pages/Tab2';
-import Tab3 from './pages/Tab3';
-
-/* Core CSS required for Ionic components to work properly */
+import { personSharp, imageSharp, homeSharp, logInSharp} from 'ionicons/icons';
+import Profile from './pages/profile';
+import Home from './pages/home';
+import Explore from './pages/explore';
 import '@ionic/react/css/core.css';
-
 /* Basic CSS for apps built with Ionic */
 import '@ionic/react/css/normalize.css';
 import '@ionic/react/css/structure.css';
@@ -32,35 +30,45 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import Login from './pages/login';
+
 
 const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonTabs>
         <IonRouterOutlet>
-          <Route exact path="/tab1">
-            <Tab1 />
+         
+          <Route exact path="/login">
+            <Login />
           </Route>
-          <Route exact path="/tab2">
-            <Tab2 />
+          <Route exact path="/profile">
+            <Profile />
           </Route>
-          <Route path="/tab3">
-            <Tab3 />
+          <Route exact path="/home">
+            <Home />
+          </Route>
+          <Route path="/explore">
+            <Explore />
           </Route>
           <Route exact path="/">
-            <Redirect to="/tab1" />
+            <Redirect to="/home" />
           </Route>
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon icon={settingsSharp} />
-            <IonLabel>Settings</IonLabel>
+          <IonTabButton tab="login" href="/login">
+            <IonIcon icon={logInSharp} />
+            <IonLabel>Log In</IonLabel>
           </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
+          <IonTabButton tab="profile" href="/profile">
             <IonIcon icon={personSharp} />
             <IonLabel>Profile</IonLabel>
           </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
+          <IonTabButton tab="home" href="/home">
+            <IonIcon icon={homeSharp} />
+            <IonLabel>Home</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="explore" href="/explore">
             <IonIcon icon={imageSharp} />
             <IonLabel>Explore</IonLabel>
           </IonTabButton>
